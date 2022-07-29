4fe799ca51ef32dd4ad9c700484b22b4e8bb45ee
ordinaryask <ordinaryask@gmail.com>
2022-07-29 12:54:33 +0200

    fix: svg react

--- src/icons/common/arrowChecked.tsx
+++ src/icons/common/arrowChecked.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 
-export default (
-  <svg
-    fill="#000000"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="10px"
-    height="10px"
-  >
-    <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z" />
-  </svg>
-)
+export default () => {
+  return (
+    <svg
+      fill="#000000"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="10px"
+      height="10px"
+    >
+      <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z" />
+    </svg>
+  )
+}
