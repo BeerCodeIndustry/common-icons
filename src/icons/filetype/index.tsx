25bf5308235b5f7f46f40fbf442a3a0aae1ec5bf
ordinaryask <ordinaryask@gmail.com>
2022-10-08 18:41:33 +0200

    refactor: delete useless

--- src/icons/filetype/index.tsx
+++ src/icons/filetype/index.tsx
@@ -1,10 +1,5 @@
 export { default as JS } from './js'
 export { default as JsonSVG } from './json'
-import { default as ReactSVG } from './react'
+export { default as ReactSVG } from './react'
 export { default as TS } from './ts'
 export { default as TXT } from './txt'
-import React from 'react'
-
-export const S = () => {
-  return <ReactSVG />
-}
