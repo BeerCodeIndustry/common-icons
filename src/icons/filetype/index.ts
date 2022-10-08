0b2abe1e8bad690fa2bad91ec515b8ccf3bcf063
ordinaryask <ordinaryask@gmail.com>
2022-10-08 17:41:01 +0200

    feat: add filetype icons

---
+++ src/icons/filetype/index.ts
@@ -0,0 +1,5 @@
+export { default as js } from './js'
+export { default as json } from './json'
+export { default as react } from './react'
+export { default as ts } from './ts'
+export { default as txt } from './txt'
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 export * as social from './icons/social'
-export * as common from './icons/common'
-export * as special from './icons/special'
 export * from './icons/social'
+
+export * as common from './icons/common'
 export * from './icons/common'
+
+export * as special from './icons/special'
 export * from './icons/special'
+
+export * as filetype from './icons/filetype'
+export * from './icons/filetype'
